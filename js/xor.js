daf8780c36cd4327e0cca6e63545d889dc880e61
Cupsa Darius <cupsadarius@gmail.com>
2018-05-03 22:35:34 +0300

    building blocks

---
+++ js/xor.js
@@ -0,0 +1,37 @@
+let nn = new NeuralNetwork(2, 4, 1);
+
+let training_data = [{
+    inputs: [0, 0],
+    outputs: [0]
+  },
+  {
+    inputs: [0, 1],
+    outputs: [1]
+  },
+  {
+    inputs: [1, 0],
+    outputs: [1]
+  },
+  {
+    inputs: [1, 1],
+    outputs: [0]
+  }
+];
+
+let randomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
+let random = (dataset) => {
+  let index = randomInt(0, dataset.length);
+  return dataset[index];
+} 
+
+nn.setLearningRate(0.3);
+
+for (let i = 0; i < 10000; i++) {
+  let data = random(training_data);
+  nn.train(data.inputs, data.outputs);
+}
